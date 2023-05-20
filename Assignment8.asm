cca517b205191051e0f19b292693eb3a8dc84735
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-05-20 14:16:32 +0530

    Update Assignment8.asm

--- Assignment8.asm
+++ Assignment8.asm
@@ -1,56 +1,55 @@
-%macro read 2
-    mov rax, 00
-    mov rdi, 00
-    mov rsi, %1
-    mov rdx, %2
-    Syscall
-%endmacro
+; ---------- QUESTION ----------
 
-%macro write 2
-    mov rax, 01
-    mov rdi, 01
-    mov rsi, %1
-    mov rdx, %2
+; Write X86/64 ALP to perform overlapped block transfer with string
+; specific instructions. Block containing data can be defined in the data
+; segment.
+
+; ---------- MACROS ----------
+
+%macro IO 4
+    mov rax, %1
+    mov rdi, %2
+    mov rsi, %3
+    mov rdx, %4
     Syscall
 %endmacro
 
 %macro hex_ascii 2
-    mov rcx, 02            ;counter
-    mov rbp, %1            ;converted data stored here
-    mov bl, %2             ;data stored to be be converted register
+    mov rcx, 02                     ;counter
+    mov rbp, %1                     ;converted data stored here
+    mov bl, %2                      ;data stored to be be converted register
 
-    %%label1:              ;hex to ascii conversion step 1
-    rol bl, 04             ;rotate by 4 bits to exchange positions of the digits
-    mov dl, bl             ;mov this rotated content to dl
-    and dl, 0Fh            ;and with 0F(0000 1111) to clear the ten's place
-    cmp dl, 09h            ;compare with 09 and add accordingly
+    %%label1:                       ;hex to ascii conversion step 1
+    rol bl, 04                      ;rotate by 4 bits to exchange positions of the digits
+    mov dl, bl                      ;mov this rotated content to dl
+    and dl, 0Fh                     ;and with 0F(0000 1111) to clear the ten's place
+    cmp dl, 09h                     ;compare with 09 and add accordingly
     jbe %%label2
-    add dl, 07h            ;if dl is greater than 09 add 37, else add only 30
+    add dl, 07h                     ;if dl is greater than 09 add 37, else add only 30
     
-    %%label2:              ;hex to ascii conversion step 2
+    %%label2:                       ;hex to ascii conversion step 2
     add dl, 30h        
-    mov [rbp], dl          ;mov the converted ascii nos to data of rbp i.e storage variable
-    inc rbp                ;increment the pointer to storage variable
-    dec rcx                ;decrement counter
-    jnz %%label1           ;do again if counter not 0
+    mov [rbp], dl                   ;mov the converted ascii nos to data of rbp i.e storage variable
+    inc rbp                         ;increment the pointer to storage variable
+    dec rcx                         ;decrement counter
+    jnz %%label1                    ;do again if counter not 0
 %endmacro
 
-%macro printarray 2
-   
-    mov rsi, %1                   ;move array base address to rsi
-    mov byte[counter], %2         ;set counter to size of array
+%macro printarray 2                 
+    mov rsi, %1                     ;move array base address to rsi
+    mov byte[counter], %2           ;set counter to size of array
 
-    %%label:                      ;displaying the array elements in new line
-    mov al, [rsi]                 ;mov data of rsi to al
-    push rsi                      ;save register data into the stack
-    hex_ascii ans,al              ;hex to ascii conversion of data in al and store it in ans variable
-    write ans,2                   ;print the converted data
-    mov byte[ans] , 10            ;store a nextline character in ans
-    write ans,1                   ;print 1 character of ans
-    pop rsi                       ;restore the top of the stack
-    inc rsi                       ;increment rsi
-    dec byte[counter]             ;decrement counter
-    jnz %%label                   ;jump till counter is not zero
+    %%label:                        ;displaying the array elements in new line
+    mov al, [rsi]                   ;mov data of rsi to al
+    push rsi                        ;save register data into the stack
+    hex_ascii ans,al                ;hex to ascii conversion of data in al and store it in ans variable
+    IO 1,1,ans,2                    ;print the converted data
+    mov byte[ans] , 10              ;store a nextline character in ans
+    IO 1,1,ans,1                    ;print 1 character of ans
+    pop rsi                         ;restore the top of the stack
+    inc rsi                         ;increment rsi
+    dec byte[counter]               ;decrement counter
+    jnz %%label                     ;jump till counter is not zero
 %endmacro
 
 %macro exit 0
@@ -59,28 +58,7 @@
     Syscall
 %endmacro
 
-blktsfwos:                       ;block transfer without string instructions
-    mov rsi, srcblk
-    mov rdi, dstblk
-    mov byte[counter] ,05
-    
-    again:
-    mov bl, [rsi]
-    mov [rdi], bl
-    inc rsi
-    inc rdi
-    dec byte[counter]
-    jnz again
-    ret
-    
-blktsfws:                        ;block transfer with string instructions
-    mov rsi, srcblk
-    mov rdi, dstblk
-    mov rcx, 05h
-    
-    cld
-    rep movsb
-    ret
+; ---------- DATA ----------
 
 section .data
 srcblk db 01h , 02h , 03h , 04h , 05h
@@ -94,32 +72,61 @@ L3 equ $-M3
 M4 db "The contents of destination block after block transfer are.." , 10
 L4 equ $-M4
 
+; ---------- BSS ----------
+
 section .bss
 ans resb 2
 counter resb 1
 
+; ---------- TEXT ----------
+
 section .text
 global _start:
-_start:
+_start: 
     
-    write M2 , L2
+    IO 1,1,M2,L2                    ; print the message
     
-    printarray srcblk , 5    
+    printarray srcblk , 5           ; print the source array  
+
+    IO 1,1,M3,L3                    ; print the message
     
-    write M3 , L3
+    printarray dstblk , 5           ; print the initial destination array
     
-    printarray dstblk , 5  
+    call blktsfwos                  ; call the procedure to perform block transfer without string instructions
     
-    call blktsfwos
+    IO 1,1,M4,L4                    ; print the message
     
-    write M4 , L4
+    printarray dstblk , 5           ; print the final destination array
     
-    printarray dstblk , 5
+    call blktsfws                   ; call the procedure to perform block transfer with string instructions
     
-    call blktsfws    
+    IO 1,1,M4,L4                    ; print the message
     
-    write M4 , L4
-    
-    printarray dstblk , 5   
+    printarray dstblk , 5           ; print the final destination array
     
     exit
+
+; ---------- PROCEDURES ----------
+
+blktsfwos:                          ;block transfer without string instructions
+    mov rsi, srcblk                 ; Move the source block address to rsi
+    mov rdi, dstblk                 ; Move the destination block address to rdi
+    mov byte[counter] ,05           ; Set the counter to 5
+    
+    again:
+    mov bl, [rsi]                   ; Move the data of rsi to bl
+    mov [rdi], bl                   ; Move the data of bl to rdi
+    inc rsi                         ; Increment rsi
+    inc rdi                         ; Increment rdi 
+    dec byte[counter]               ; Decrement counter
+    jnz again                       ; Jump till counter is not zero
+    ret                                                 
+    
+blktsfws:                            ;block transfer with string instructions
+    mov rsi, srcblk                  ; Move the source block address to rsi   
+    mov rdi, dstblk                  ; Move the destination block address to rdi
+    mov rcx, 05h                     ; Set the counter to 5
+    
+    cld                              ; Clear the direction flag
+    rep movsb                        ; Move the data from rsi to rdi
+    ret
