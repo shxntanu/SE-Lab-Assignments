5a742c9b9dffad20a992cd05c01dedac5b655974
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-05-20 13:55:59 +0530

    Update Assignment7.asm

--- Assignment7.asm
+++ Assignment7.asm
@@ -1,4 +1,6 @@
-%macro scall 4
+; ---------- MACROS ----------
+
+%macro IO 4
     mov rax, %1
     mov rdi, %2
     mov rsi, %3
@@ -6,7 +8,10 @@
     syscall
 %endmacro
 
+; ----------- DATA -----------
+
 section .data
+
     Prot db "Protected Mode : ",10
     Protlen equ $-Prot
     
@@ -27,106 +32,125 @@ section .data
     
     ld db 10,"LDTR : "
     ldlen equ $-ld
+
+; ----------- BSS -----------
     
 section .bss
-    gdt resb 8          ;to hold the value of the base address
-    gdtli resb 2        ;to hold the value of the limit field
-    msw1 resb 2         ;to hold msw value
-    temp resb 1         ;to temporary store the value of al register
+    gdt resb 8                      ; to hold the value of the base address of GDT
+    gdtli resb 2                    ; to hold the value of the limit field of GDT
+    msw1 resb 2                     ; to hold msw value
+    temp resb 1                     ; to store the value of al register temporarily
     result1 resq 1      
     result2 resw 1
-    idt1 resb 8         ;to hold the value of the base address
-    idtli resb 2        ;to hold the value of the limit field
-    ldt resb 2          ;to hold ldtr value
-    t_r resb 2          ;to hold the tr value
+    idt1 resb 8                     ; to hold the value of the base addressm of IDT
+    idtli resb 2                    ; to hold the value of the limit field of IDT
+    ldt resb 2                      ; to hold ldtr value
+    t_r resb 2                      ; to hold the tr value
+
+; ----------- TEXT -----------
 
 section .text
     global _start
+
 _start:
     mov rsi,msw1
     smsw [rsi]
     mov ax,[rsi]
-    bt ax,0                ;if carry = 1 then it is protected
-    jc next
-    scall 1,1,Real,Reallen   
+    bt ax,0                         ; If PE bit of MSW = 1 then it is protected mode
+    jc next                         ; Print Protected Mode
+    IO 1,1,Real,Reallen             ; Print Real Mode
     jmp z1
-next: scall 1,1,Prot,Protlen 
+
+next: 
+    IO 1,1,Prot,Protlen 
+
 z1:
-    scall 1,1,msw,mswlen ;display the contents of msw
+    IO 1,1,msw,mswlen               ; Display the contents of MSW (16 bit)
     mov ax,word[msw1]
     call display2 
     
-    scall 1,1,gdtr,gdtrlen
+    IO 1,1,gdtr,gdtrlen
     mov rsi,gdt
     sgdt [rsi]
     mov rax, qword[rsi]
-    call display1       ;display the contents of gdtr
+    call display1                   ; Display the contents of GDTR (64 bit)
     
     mov rsi,gdtli
     mov ax,word[rsi]
-    call display2       ;display the table limit of gdtr
+    call display2                   ; Display the table limit of GDTR (16 bit)
     
-    scall 1,1,ld,ldlen
+    IO 1,1,ld,ldlen
     mov rsi,ldt
     sldt [rsi]
     mov rax, [ldt]
-    call display2       ;display the contents of ldtr
+    call display2                   ; Display the contents of LDTR (16 bit)
     
-    scall 1,1,idt,idtlen
+    IO 1,1,idt,idtlen
     mov rsi,idt1
     sidt [rsi]
     mov rax, [idt1]
-    call display1       ;display the contents of idtr
+    call display1                   ; Display the contents of IDTR (64 bit)
     
     mov ax,[idtli]
-    call display2       ;display the table limit of idtr
+    call display2                   ; Display the table limit of IDTR (16 bit)
     
-    scall 1,1,tr,trlen
+    IO 1,1,tr,trlen
     mov rsi,t_r
     str [rsi]
     mov rax, [t_r]
-    call display2       ;display the contents of tr 
+    call display2                   ; Display the contents of TR (16 bit) 
     
-    scall 60,0,0,0 
+    ; Exit System Call
+    IO 60,0,0,0 
+
+; ----------- PROCEDURE -----------
     
 display1:
-    mov bp,16              ;to display 64 bit no (same process as we have done earlier)
-up1:
-    rol ax,4
-    mov qword[result1],rax
-    and al,0fh
-    cmp al,09h
-    jbe next1
-    add al,07h
-next1:
-    add al,30h
-    mov byte[temp],al
-    scall 1,1,temp,1
-    mov rax,qword[result1]
-    dec bp
-    jnz up1
-    ret
 
-display2:
-    mov bp,4                ;to display 16 bit no (same process as we have done earlier)
-up2:
-    rol ax,4
-    mov word[result2],ax
-    and al,0fh
-    cmp al,09h
-    jbe next2
-    add al,07h
-next2:
-    add al,30h
-    mov byte[temp],al
-    scall 1,1,temp,1
-    mov ax,word[result2]
-    dec bp
-    jnz up2
-    ret
+        mov bp,16                   ; To display 64 bit no
+    up1:
+        rol ax,4
+        mov qword[result1],rax
+        and al,0fh
+        cmp al,09h
+        jbe next1
+        add al,07h
+    next1:
+        add al,30h
+        mov byte[temp],al
+        IO 1,1,temp,1
+        mov rax,qword[result1]
+        dec bp
+        jnz up1
+        ret
+
+display2:  
+                                    
+        mov bp,4                    ; To display 16 bit no (same process as we have done earlier) 
+    up2:
+        rol ax,4
+        mov word[result2],ax
+        and al,0fh
+        cmp al,09h
+        jbe next2
+        add al,07h
+    next2:
+        add al,30h
+        mov byte[temp],al
+        IO 1,1,temp,1
+        mov ax,word[result2]
+        dec bp
+        jnz up2
+        ret
+
+; ----------- TO RUN -----------
     
-;nasm -f elf64 mp07.asm && ld -o mp07 mp07.o && ./mp07
-;output:
+; nasm -f elf64 <filename>.asm
+; ld -o <filename> <filename>.o
+; ./<filename>
+
+; ----------- OUTPUT -----------
+
 ;Protected Mode : 
 ;MSW  : 0033
 ;GDTR : 007F007F007F007FFFFF
