76fce6457c605c62c2e84bdae7a167963a66dc81
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-05-25 10:02:31 +0530

    Update mp09_2.asm

--- Assignment09/mp09_2.asm
+++ Assignment09/mp09_2.asm
@@ -18,137 +18,77 @@ global main2
 
 main2:
 
-
 global spaces,enters,occ
 
-
-
 spaces:
-
 mov rsi,buffer
 
-
 up:
-
 mov al, byte[rsi]
-
 cmp al,20H
-
 je next3
-
 inc rsi
-
 dec byte[cnt]
-
 jnz up
-
 
 jmp next4
 
-
 next3:
-
 inc rsi
-
 inc byte[scount]
-
 dec byte[cnt]
-
 jnz up
 
-
 next4:
-
 add byte[scount], 30h
-
 scall 1,1,scount, 2
-
 scall 1,1,new,new_len
 ret
 
 enters:
-
 mov rsi,buffer
 
 up2:
-
 mov al, byte[rsi]
-
 cmp al,0AH
-
 je next5
-
 inc rsi
-
 dec byte[cnt2]
-
 jnz up2
-
-
 jmp next6
 
 next5:
-
 inc rsi
-
 inc byte[ncount]
-
 dec byte[cnt2]
-
 jnz up2
 
-
 next6:
-
 add byte[ncount], 30h
-
 scall 1,1,ncount, 2
-
 scall 1,1,new,new_len
 ret
 
-
-
 occ:
-
 mov rsi,buffer
-
 up3:
-
 mov al, byte[rsi]
-
 cmp al,bl
-
-
 je next7
-
 inc rsi
-
 dec byte[cnt3]
-
 jnz up3
-
 jmp next8
 
-
 next7:
-
 inc rsi
-
 inc byte[chacount]
-
 dec byte[cnt3]
-
 jnz up3
 
-
 next8:
-
 add byte[chacount], 30h
-
 scall 1,1,msg6,len6
-
 scall 1,1,chacount, 1
-
 scall 1,1,new,new_len
 ret
