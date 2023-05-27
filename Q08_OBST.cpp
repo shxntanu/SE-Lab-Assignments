450759ec18c5eeb880c6b88f7c446086bbf4ab50
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-05-27 15:32:58 +0530

    Update Q08_OBST.cpp

--- Q08_OBST.cpp
+++ Q08_OBST.cpp
@@ -5,6 +5,7 @@ Given sequence k = k1 <k2 < … < kn of n sorted keys, with a search
 probability pi for each key ki . Build the Binary search tree that has
 the least search cost given the access probability for each key.
 */
+
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 20
@@ -32,15 +33,15 @@ class Node
 
 class OBST
 {
-	double C[MAX][MAX];
-	double W[MAX][MAX];
-	int R[MAX][MAX];
-	double  q[MAX];
-   double p[MAX];
+	double C[MAX][MAX];  // Matrix to store the cost of optimal binary search trees
+	double W[MAX][MAX];  // Matrix to store the weight (sum of access probabilities) of subtrees
+	int R[MAX][MAX];     // Matrix to store the roots of optimal subtrees
+	double q[MAX];       // Array to store the access probabilities of dummy keys
+	double p[MAX];       // Array to store the access probabilities of actual keys
 
-	int n;
-	int keys[MAX];
-	Node *root;
+	int n;               // Number of keys
+	int keys[MAX];       // Array to store the actual keys
+	Node *root;          // Root node of the constructed optimal binary search tree
 
   public:
 	OBST()
@@ -56,18 +57,19 @@ class OBST
 
 		for (i = 0; i <= n; i++)
 		{
-			W[i][i] = q[i];
+			W[i][i] = q[i];  // Weight of a single node is its access probability (dummy key or actual key)
 			for (j = i + 1; j <= n; j++)
-				W[i][j] = W[i][j - 1] + p[j] + q[j];
+				W[i][j] = W[i][j - 1] + p[j] + q[j];  // Weight of a subtree is the sum of its nodes' access probabilities
+
 		}
 
 		for (i = 0; i <= n; i++)
-			C[i][i] = W[i][i];
+			C[i][i] = W[i][i];  // Cost of a single node is its weight
 		for (i = 0; i <= n - 1; i++)
 		{
 			j = i + 1;
-			C[i][j] = C[i][i] + C[j][j] + W[i][j];
-			R[i][j] = j;
+			C[i][j] = C[i][i] + C[j][j] + W[i][j];  // Cost of a subtree with two nodes is the sum of the nodes' weights and the root weight
+			R[i][j] = j;  // Root of the subtree with two nodes is the second node
 		}
 		for (h = 2; h <= n; h++)
 			for (i = 0; i <= n - h; i++)
@@ -84,8 +86,8 @@ class OBST
 						min = x;
 					}
 				}
-				C[i][j] = W[i][j] + min;
-				R[i][j] = m;
+				C[i][j] = W[i][j] + min;  // Cost of the subtree is the sum of its weight and the minimum cost of its left and right subtrees
+				R[i][j] = m;  // Root of the subtree is the node that gives the minimum cost
 			}
 
 		cout << "\nThe weight matrix W:\n";
@@ -122,9 +124,9 @@ class OBST
 		else
 		{
 			p = new Node();
-			p->key = keys[R[i][j]];
-			p->left = Construct_OBST(i, R[i][j] - 1);
-			p->right = Construct_OBST(R[i][j], j);
+			p->key = keys[R[i][j]];  // Root node key is determined by the optimal root index stored in R[i][j]
+			p->left = Construct_OBST(i, R[i][j] - 1);  // Construct left subtree recursively
+			p->right = Construct_OBST(R[i][j], j);  // Construct right subtree recursively
 		}
 		return p;
 	}
@@ -136,6 +138,7 @@ class OBST
 			display(ROOT->left, nivel + 1);
 			cout << ROOT->key<< endl;
 			display(ROOT->right, nivel + 1);
+			// Display the tree in an inorder traversal (left subtree, root, right subtree)
 		}
 	}
 	void obst()
@@ -149,7 +152,7 @@ class OBST
 
 		cout << "The Least cost is: \n"
 			 << C[0][n]<<endl;
-		root = Construct_OBST(0, n);
+		root = Construct_OBST(0, n);  // Construct the optimal binary search tree using the root matrix R
 	}
 	void inp()
 	{
@@ -174,27 +177,29 @@ class OBST
 		return root;
 	}
 };
+
 int main()
 {
 	int k;
 
-	OBST ob;
+	OBST tree;
 
-	while (1)
+	while (true)
 	{
 		cout << "1.Construct tree\n2.Display tree\n3.Exit\n";
 		cin >> k;
 		switch (k)
 		{
 		case 1:
-		    ob.inp();
-			ob.obst();
+		    tree.inp();  // Input the number of keys, their access probabilities, and dummy key access probabilities
+			tree.obst();  // Calculate the optimal binary search tree and construct it
 			break;
 		case 2:
-			ob.display(ob.get(), 0);
+			tree.display(tree.get(), 0);  // Display the constructed tree
 			break;
 		}
 		if(k==3)
 			break;
 	}
+	return 0;
 }
