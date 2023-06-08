13e94451a8925f6b9cd474e2aee862ca3c85927e
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-06-08 14:06:27 +0530

    Update Q10_HeapSort.cpp

--- Q10_HeapSort.cpp
+++ Q10_HeapSort.cpp
@@ -1,5 +1,5 @@
 /*
-Credits - Shubham Panchal (GitHub -- @shubham0204)
+Source - Shubham Panchal (GitHub -- @shubham0204)
 
 Implement the Heap Sort algorithm demonstrating the Heap / Shell data structure with 
 modularity of programming language.
